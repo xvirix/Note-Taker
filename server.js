130b29d57c858fd3b94a0d41436ef5c01a0743fd
xvirix <77359420+xvirix@users.noreply.github.com>
2021-04-24 10:30:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-
 const allNotes = require('./db/db.json');
 
 //Routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -26,6 +21,10 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
 
 //saving notes
