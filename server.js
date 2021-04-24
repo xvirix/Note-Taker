0f767853c892d28db187e1e9a3451cde97b3e4d6
xvirix <77359420+xvirix@users.noreply.github.com>
2021-04-24 03:29:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
