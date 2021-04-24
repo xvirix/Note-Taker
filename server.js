d9f43f67173047abba4349d7bd1e5e383960e71f
xvirix <77359420+xvirix@users.noreply.github.com>
2021-04-24 03:21:51 -0500

    fixed errors

--- server.js
+++ server.js
@@ -1,73 +1,52 @@
 //Dependancies
 const express = require('express');
-const app = express();
-const PORT = 80;
 const fs = require('fs');
+const app = express();
 const path = require('path');
-const allNotes = require('./db/db.json');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
 
-// Routes
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+const PORT = process.env.PORT || 3000;
+
+//routes
+// saves notes to db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Functions
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-
+//posting
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
+    const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+})
 
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
-
+//deleting
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNotes = notes.filter((removeNote) => removeNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+})
 
 //Listener
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, function () {
+    console.log("App listening to PORT: " + PORT);
+});
